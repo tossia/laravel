fe780d119f013910fe94eb716566352101431986
tossia <tossia2000@yahoo.fr>
2021-11-16 23:46:16 +0100

    factories are done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,57 +15,20 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        Post::factory()->create();
-        /*
-          $user = User::factory()->create();
-          $personal = Category::create([
-          'name' => 'Personal',
-          'slug' => 'personal'
-          ]);
-
-          $work = Category::create([
-          'name' => 'Work',
-          'slug' => 'work'
-          ]);
-
-          $hobbies = Category::create([
-          'name' => 'Hobbies',
-          'slug' => 'hobbies'
-          ]);
-
-          Post::create([
-          'user_id' => $user->id,
-          'category_id' => $personal->id,
-          'title' => 'My first post',
-          'excerpt' => 'Excerpt for my first post',
-          'slug' => 'my-personal-post',
-          'body' => '<p>Something interessting for my first post</p>'
-          ]);
-
-          Post::create([
-          'user_id' => $user->id,
-          'category_id' => $work->id,
-          'title' => 'My second post',
-          'excerpt' => 'Excerpt for my second post',
-          'slug' => 'my-work-post',
-          'body' => '<p>Super interessting for my work post<p>'
-          ]);
-
-          Post::create([
-          'user_id' => $user->id,
-          'category_id' => $hobbies->id,
-          'title' => 'My third post',
-          'excerpt' => 'Excerpt for my third post',
-          'slug' => 'my-hobbies-post',
-          'body' => '<p>Amazing interessting for my hobby post</p>'
-          ]);
-         * 
+        /* Can be deleted if the creation is done after refresh migration
+          User::truncate();
+          Category::truncate();
+          Post::truncate();
          */
+        //Post::factory(5)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Smith'
+        ]);
+
+        Post::factory([
+        'user_id' => $user->id
+        ])
     }
 
 }
