66d52ae2f758449a1238024a01cceb1b8431dac4
tossia <tossia2000@yahoo.fr>
2021-12-02 22:35:06 +0100

    37 research 1

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,16 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
 
     return view('posts', [
-'posts' => Post::latest('created_at')->get(),
+'posts' => $posts->get(),
  'categories' => Category::all()
     ]);
 })->name('home');
